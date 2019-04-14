6e7978e419b07a147493c3d36917ee6bdd67ae7f
Marius <marius@twostairs.com>
2019-04-15 00:46:03 +0200

    Added port through Confex, added ip

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,8 @@ config :paperwork_service_configs, Paperwork.Server,
     adapter: Plug.Cowboy,
     plug: Paperwork,
     scheme: :http,
-    port: 8880
+    ip: {0,0,0,0},
+    port: {:system, :integer, "PORT", 8880}
 
 config :paperwork_service_configs,
     maru_servers: [Paperwork.Server]
